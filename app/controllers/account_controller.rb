7ea034ac4b31898ca78e7313033f0f567b17c823
LiuSihao <liusihao123@qq.com>
2013-05-06 10:14:39 +0800

    add sign_in and sing_up partial in home page

---
+++ app/controllers/account_controller.rb
@@ -0,0 +1,56 @@
+# coding: utf-8
+class AccountController < Devise::RegistrationsController
+  def edit
+    @user = current_user
+    # 首次生成用户 Token
+    @user.update_private_token if @user.private_token.blank?
+  end
+
+  def update
+    self.resource = resource_class.to_adapter.get!(send(:"current_#{resource_name}").to_key)
+
+    # Can update Email when email was not has being exist.
+    resource.email = resource_params[:email] if self.resource.email.blank?
+
+    # code from Devise
+    if resource.update_with_password(resource_params)
+      if is_navigational_format?
+        if resource.respond_to?(:pending_reconfirmation?) && resource.pending_reconfirmation?
+          flash_key = :update_needs_confirmation
+        end
+        set_flash_message :notice, flash_key || :updated
+      end
+      sign_in resource_name, resource, :bypass => true
+      respond_with resource, :location => after_update_path_for(resource)
+    else
+      clean_up_passwords resource
+      respond_with resource
+    end
+  end
+
+  # POST /resource
+  def create
+    build_resource
+    resource.email = params[resource_name][:email]
+    if resource.save
+      if resource.active_for_authentication?
+        set_flash_message :notice, :signed_up if is_navigational_format?
+        sign_in(resource_name, resource)
+        respond_with resource, :location => after_sign_up_path_for(resource)
+      else
+        set_flash_message :notice, :"signed_up_but_#{resource.inactive_message}" if is_navigational_format?
+        expire_session_data_after_sign_in!
+        respond_with resource, :location => after_inactive_sign_up_path_for(resource)
+      end
+    else
+      clean_up_passwords resource
+      respond_with resource
+    end
+  end
+
+  def destroy
+    resource.soft_delete
+    sign_out_and_redirect("/sign_in")
+    set_flash_message :notice, :destroyed
+  end
+end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,16 @@
 module ApplicationHelper
+  def resource_name
+    :user
+  end
+
+  def resource
+    @resource ||= User.new
+  end
+
+  def devise_mapping
+    @devise_mapping ||= Devise.mappings[:user]
+  end
+
   def follow_btn
     link_to('follow', follow_user_path(@user), remote: true, class: 'btn btn-primary')
   end
