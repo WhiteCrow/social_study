2e0aa87833c4f59e5a8974778a42ac36b7f1064f
LiuSihao <liusihao123@qq.com>
2013-05-21 14:37:37 +0800

    completed test and controller part of create and destroy comment

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -7,8 +7,8 @@ class Comment
   belongs_to :commentable, :polymorphic => true
   belongs_to :user
 
-  attr_accessible :commentable_type, :commentable_id, :content
-  validates_presence_of :commentable, :content, :user
+  attr_accessible :commentable_type, :commentable_id, :content, :commentable, :user_id, :user
+  validates_presence_of :content, :commentable_type, :commentable_id, :user_id
 
   index :user_id => 1
   index :commentable_type => 1
