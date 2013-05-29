a2ba75cd2b474e229a674ba8775337fe2fb17193
LiuSihao <liusihao123@qq.com>
2013-05-29 23:04:02 +0800

    add links to microblog

--- app/models/microblog.rb
+++ app/models/microblog.rb
@@ -2,7 +2,9 @@ class Microblog
   include Mongoid::Document
   include Mongoid::Timestamps::Created
 
+  has_many :comments, as: :commentable, dependent: :destroy
   belongs_to :user
+
   attr_accessible :content, :user_id, :user
   validates_presence_of :content, :user_id
 
