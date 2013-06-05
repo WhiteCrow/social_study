45f1522fb38319a05fdb3c0d22b950de368d0e49
LiuSihao <liusihao123@qq.com>
2013-06-05 14:08:00 +0800

    remove init file : mongoid-audit.rb

--- config/initializers/mongoid-audit.rb
+++
@@ -1,6 +0,0 @@
-# config/initializers/mongoid-audit.rb
-# initializer for mongoid-audit
-# assuming HistoryTracker is your tracker class
-Mongoid::Audit.tracker_class_name = :audit
-Mongoid::Audit.current_user_method = :current_user
-require_dependency 'audit.rb'
