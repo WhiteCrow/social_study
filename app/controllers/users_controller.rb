6138844c28f521de8a757c5eced68121e06ec6d8
LiuSihao <liusihao123@qq.com>
2013-06-19 16:41:21 +0800

    change microblog index to states index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -40,7 +40,7 @@ class UsersController < ApplicationController
 
   def states
     @user = User.find(params[:id])
-    @states = Audit.state.desc('created_at').page(params[:page]||1).per(20)
+    @states = Audit.states.in(modifier: current_user).desc('created_at').page(params[:page]||1).per(20)
     respond_to do |format|
       format.html
       format.js { render 'states/paginate' }
