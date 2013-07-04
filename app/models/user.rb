30f844bcd6459f16f9bfa42a8f074df8944a077a
LiuSihao <liusihao123@qq.com>
2013-07-04 22:53:50 +0800

    change relay UI

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,6 +23,7 @@ class User
   validates_presence_of :name, :encrypted_password, :role
   validates_uniqueness_of :name, :email, :case_sensitive => false
   validates_inclusion_of :role, in: ROLES
+  validates_format_of :email, with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
 
   def role?(target_role)
     self.role == target_role.to_sym||target_role.to_s
