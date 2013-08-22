d83b42ed1de97b2c00134b62071c15b1d0c361fa
LiuSihao <liusihao123@qq.com>
2013-08-22 11:19:38 +0800

    fix update entry test

--- spec/features/entries_spec.rb
+++ spec/features/entries_spec.rb
@@ -7,12 +7,12 @@ describe "Entry", type: :feature do
     visit "/users/#{user.id}"
     within '#current-entry .active' do
       find('.edit-entry').click
-      page.driver.render('tmp/capybara/entry.png', :full => true)
       page.should have_selector "form"
       fill_in "entry_title", with: "The new entry"
       fill_in "entry_content", with: "New entry's content"
       click_button "提交"
-      #page.should_not have_selector "form"
+      page.should_not have_selector "form"
+      #page.driver.render('tmp/capybara/entry.png', :full => true)
       page.should have_content "New entry's content"
     end
     within "#entry-previous" do
