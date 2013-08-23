d43586ea011b57528ccc991c858caaa8238079d6
LiuSihao <liusihao123@qq.com>
2013-08-23 10:00:14 +0800

    comment cancel edit entry test

--- spec/features/entries_spec.rb
+++ spec/features/entries_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 
 describe "Entry", type: :feature do
   include Login
-  it 'edit', js: true do
+  it 'edit and update', js: true do
     visit "/users/#{user.id}"
     within '#current-entry .active' do
       find('.edit-entry').click
@@ -19,5 +19,24 @@ describe "Entry", type: :feature do
       page.should have_content "The new entry"
     end
   end
+
+  #FIXME will confirm alert dialog when user cancel edit entry.
+  #it 'edit and cancel', js: true do
+  #  visit "/users/#{user.id}"
+  #  within '#current-entry .active' do
+  #    find('.edit-entry').click
+  #    page.should have_selector "form"
+  #    fill_in "entry_title", with: "The new entry"
+  #    fill_in "entry_content", with: "New entry's content"
+  #    click_link "取消"
+  #    #click_button "确定"
+  #    page.should_not have_selector "form"
+  #    page.driver.render('tmp/capybara/entry.png', :full => true)
+  #    page.should have_content "默认条目"
+  #  end
+  #  within "#entry-previous" do
+  #    page.should have_content "默认条目"
+  #  end
+  #end
 end
 
