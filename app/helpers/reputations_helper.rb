b334d0008f52c8eaad1e2733528d1e8a1e4d7f9b
LiuSihao <liusihao123@qq.com>
2013-09-07 18:41:15 +0800

    tmp

--- app/helpers/reputations_helper.rb
+++ app/helpers/reputations_helper.rb
@@ -44,23 +44,4 @@ module ReputationsHelper
       span.concat(raw '&nbsp;').concat(div)
     end
   end
-
-
-  #def vote_path(reputable, vote_type)
-  #  reputable_id = reputable.id
-  #  reputable_type = reputable.class.name
-  #  vote_user_path(reputable_id: reputable_id, reputable_type: reputable_type, vote_type: vote_type)
-  #end
-
-  #def study_path(reputable, study_type)
-  #  reputable_id = reputable.id
-  #  reputable_type = reputable.class.name
-  #  study_user_path(reputable_id: reputable_id, reputable_type: reputable_type, study_type: study_type)
-  #end
-
-  #def grade_path(reputable, grade_type)
-  #  reputable_id = reputable.id
-  #  reputable_type = reputable.class.name
-  #  grade_user_path(reputable_id: reputable_id, reputable_type: reputable_type, grade_type: grade_type)
-  #end
 end
