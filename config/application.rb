b659dcd607e83c3713d827105ca5c9ae1dcf6bc1
LiuSihao <liusihao123@qq.com>
2013-09-15 00:12:55 +0800

    change assets precompile config

--- config/application.rb
+++ config/application.rb
@@ -19,7 +19,7 @@ module SocialStudy
   class Application < Rails::Application
 
     config.autoload_paths += %W(#{config.root}/lib)
-    config.assets.initialize_on_precompile = false
+    #config.assets.initialize_on_precompile = false
 
     config.generators do |g|
       g.view_specs false
