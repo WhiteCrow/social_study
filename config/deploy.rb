a3d0c02c6c66a2ca96eae5479c3e99975d9c94ee
LiuSihao <liusihao123@qq.com>
2013-09-14 23:29:53 +0800

    add unicorn log

--- config/deploy.rb
+++ config/deploy.rb
@@ -36,7 +36,7 @@ task :environment do
   # invoke :'rbenv:load'
 
   # For those using RVM, use this to load an RVM version@gemset.
-  invoke :'rvm:use[2.0.0p247@default]'
+  invoke :'rvm:use[2.0.0]'
 end
 
 # Put any custom mkdir's in here for when `mina setup` is ran.
