407be9c9b0fed1bc35513ead023b60a6f4a04b49
LiuSihao <liusihao123@qq.com>
2013-09-15 10:49:35 +0800

    add microblog in user page

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,9 +1,9 @@
 module UsersHelper
   def whos(string)
     if current_user.present? and current_user == @user
-      '我的' + string
+      '我' + string
     else
-      @user.name + '的' + string
+      @user.name + string
     end
   end
 end
